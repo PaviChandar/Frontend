1cd5f363058227b8f1f5296c395903138bd4fc19
PaviChandar <96729523+PaviChandar@users.noreply.github.com>
2022-04-19 12:57:59 +0530

    Update newform.js

--- newform/newform.js
+++ newform/newform.js
@@ -5,6 +5,16 @@ var Form4 = document.getElementById("Form4");
 var Form5 = document.getElementById("Form5");
 var Form6 = document.getElementById("Form6");
 
+var Back1 = document.getElementById("Back1");
+var Back2 = document.getElementById("Back2");
+var Back3 = document.getElementById("Back3");
+var Back4 = document.getElementById("Back4");
+var Next1 = document.getElementById("Next1");
+var Next2 = document.getElementById("Next2");
+var Next3 = document.getElementById("Next3");
+var Next4 = document.getElementById("Next4");
+var Submit1 = document.getElementById("Submit1");
+
 Back1.onclick = function(){
     Form1.style.left = "40px";
     Form2.style.left = "450px";
@@ -16,11 +26,13 @@ Back2.onclick = function(){
     Form3.style.left = "450px";
     progress.style.width = "160px";
 }
+
 Next3.onclick = function(){
     Form3.style.left = "-450px";
     Form4.style.left = "40px";
     progress.style.width = "200px";
 }
+
 Back3.onclick = function(){
     Form3.style.left = "40px";
     Form4.style.left = "450px";
@@ -33,100 +45,112 @@ Back4.onclick = function(){
     progress.style.width = "320px";
 }
 
-var field1=0;
-var field2=0;
-var field3=0;
-var field4=0;
-var field5=0;
+var mailfield=0;
+var passwordfield=0;
+var confirmpasswordfield=0;
+var firstnamefield=0;
+var lastnamefield=0;
 
 function validateMail(){
     var emailregex=/^[a-zA-Z0-9+_.-]+@[a-zA-Z.-]+.[a-z]{2,3}$/;
-    var email=document.getElementById("mail").value;
-    if(email=="")
+    var email = document.getElementById("mail").value;
+    if (email == "")
     {
     alert("Enter email");
-    field1=0;
+    mailfield=0;
     return false;
     }
     else if(!emailregex.test(email))
     {
         alert("Enter a valid email");
-    field1=0;
+    mailfield=0;
     return false;
     }
     else{
-        field1=1;
+        mailfield=1;
     }
 }
-function validatePass(){
-    var pswd=document.getElementById("password").value;
-    if(pswd=="")
+function validatePassword(){ 
+
+    var passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-z0-9!@#$%^&*]{6,15}$/;
+    var passWord = document.getElementById("password").value;
+    if (passWord == "")
     {
     alert("Enter password");
+    passwordfield=0;
     return false;
-    field4=0;
+  
+    }
+    else if (!passwordRegex.test(passWord))
+    {
+        alert("Enter a valid password");
     }
     else{
-        field4=1;
+        passwordfield=1;
     }
 }
 
-function validateConfirm(){
-    var cfrmpass=document.getElementById("cfrmpass").value;
-    var pswd=document.getElementById("password").value;
-    if(pswd != cfrmpass)
+function validateConfirmPassword(){
+    var confirmPassword=document.getElementById("confirmpass").value;
+    var passWord=document.getElementById("password").value;
+    if (passWord != confirmPassword)
     {
         alert("Passwords didn't match");
+       confirmpasswordfield=0;
         return false;
-        field5=0;
+        
     }
     else{
-        field5=1;
+        confirmpasswordfield=1;
     }
 }
 
 function validateName()
 {
-    var fname=document.getElementById("fname").value;
+    var firstname=document.getElementById("firstname").value;
     var nameregex=/^[a-zA-Z]{3,15}$/;
 
-    if(!nameregex.test(fname))
+    if (!nameregex.test(firstname))
     {
-    alert("name should be in character");
+    alert("Name should be in character");
+    firstnamefield=0;
     return false;
-    field2=0;
+    
     } 
     else{
-        field2=1;
+        firstnamefield=1;
     }
 }
 
-function validateLname(){
-    var lname=document.getElementById("lname").value;
-    var nameregex=/^[a-zA-Z]{3,15}$/;
-        if(lname == "")
-        {
-            alert("enter the last name");
-        return false; 
-        field3=0; 
-        }
+function validateLastname(){
+    var lastname=document.getElementById("lastname").value;
+    var nameregex=/^[a-zA-Z]{0,15}$/;
+        // if (lastname == "")
+        // {
+        //     alert("enter the last name");
+        //     lastnamefield=0;
+        //     return false; 
+         
+        // }
 
-        if(!nameregex.test(lname))
+        if (!nameregex.test(lastname))
         {
         alert("name should be in character");
+        lastnamefield=0;
         return false;
-        field3=0;
+       
         }
         else{
-            field3=1;
+            lastnamefield=1;
         }
 }
 
 
 function pageChange1()
 {
-    if(field1==1 && field2==1 && field3==1 && field4==1 && field5==1 )
+    if (mailfield==1 && passwordfield==1 && confirmpasswordfield==1 && firstnamefield==1 )
     {
+        var Next1=document.getElementById("Next1");
         Next1.onclick = function(){
                 Form1.style.left = "-450px";
                 Form2.style.left = "40px";
@@ -139,21 +163,22 @@ function pageChange1()
 }
 
 
-var field21=0;
-var field22=0;
-var field23=0;
+var dobfield=0;
+var phonefield=0;
+var statefield=0;
 
 function validateDob()
 {
 
-    if(validateAge()<18)
+    if (validateAge()<18)
     {
         alert("Age should be greater than 18");
+        dobfield=0;
         return false;
-        field21=0;
+       
     }
     else{
-        field21=1;
+        dobfield=1;
     }
 }
 function validateAge()
@@ -172,36 +197,39 @@ function validateState()
     if(stateVal=="Select a state")
         {
         alert("select a state");
+        statefield=0;
         return false;
-        field22=0;
+        
         }
         else{
-            field22=1;
+            statefield=1;
         }
 }
 function validatePhone()
 {
     var phoneNo=document.getElementById("phonenum").value;
     var phoneregex=/^[6-9]{1}[0-9]{9}$/
-        if(phoneNo == "")
+        if (phoneNo == "")
         {
         alert("enter phone no.");
+        phonefield=0;
         return false;
-        field23=0;
+       
         }
       
         else if(!phoneregex.test(phoneNo))
         {
             alert("Number should start with 6/7/8/9 and contain 10 digits");
-            field23=0;
+            phonefield=0;
         }
         else{
-            field23=1;
+            phonefield=1;
         }
 }
 function pageChange2(){
-    if(field21==1 && field22==1 && field23==1)
+    if (dobfield==1 && phonefield==1 && statefield==1)
     {
+        var Next2=document.getElementById("Next2");
         Next2.onclick = function(){
                 Form2.style.left = "-450px";
                 Form3.style.left = "40px";
@@ -215,45 +243,76 @@ else{
 
 
 
-var field41 = 0;
-var field42 = 0;
+var gradefield = 0;
+var yearofpassingfield = 0;
 
+function validateEducation() {
+    validateGrade();
+    validateYearOfPassing();
+    pageChange4()
+}
 
 function validateGrade()
 {
-    var grade=document.getElementById("grades").value;
-    if(grade=="Select a grade")
+    var grade;
+    let checkBox = document.querySelectorAll('.check-box');
+    console.log(checkBox)
+    checkBox.forEach(c => {
+        if(c.checked == true) {
+            grade = true
+        }
+    })
+    if (grade == false)
         {
         alert("select a grade");
+        gradefield = 0;
         return false;
-        field41=0;
+       
         }
         else{
-            field41=1;
+            gradefield = 1; 
         }
 }
 
-function validateYOP() {
+function validateTermsAndConditions()
+{
+    var check = document.getElementById("tnc").checked;
+    if (check == false)
+    {
+        alert("Please accept terms and conditions");
+        conditionfield=0;
+        return false;
+    }
+    else{
+        conditionfield=1;
+    }
+
+}
+
+function validateYearOfPassing() {
     var yopFormat = /^(0[1-9]|1[012])[\/\-](19[5-9]\d|20[0-2]\d)$/;
     var yop = document.getElementById("pass").value;
-    if(yop==""){
+    if (yop == ""){
         alert("This field is required");
+        yearofpassingfield=0;
         return false;
-        field42=0;
+        
     }
     else if(!yopFormat.test(yop))
     {
         alert("Enter a valid pattern");
+        yearofpassingfield=0;
         return false;
-        field42=0;
+        
     }
     else{
-        field42=1;
+        yearofpassingfield=1;
     }
 }
 
 function pageChange4() {
-    if(field41 == 1 && field42 == 1){
+    if (gradefield == 1 && yearofpassingfield == 1){
+        var Next4 = document.getElementById("Next4");
         Next4.onclick = function(){
                 Form4.style.left = "-450px";
                 Form5.style.left = "40px";
@@ -266,41 +325,41 @@ function pageChange4() {
 }
 
 var checkBox = document.getElementById('check');
-var field51=0;
-var field52=0;
-function validateDept()
+var departmentfield = 0;
+var conditionfield = 0;
+function validateDepartment()
 {
-    var dept = document.getElementById('dept').value;
-    console.log(dept);
-    if(dept=="Select a Department")
+    var department = document.getElementById('dept').value;
+    
+    if (department=="Select a Department")
     {
     alert("select a Department");
+    departmentfield=0;
     return false;
-    field51=0;
+   
     }
     else{
-        field51=1;
+        departmentfield=1;
     } 
 }
 
-function validateCheck()
+function validateTermsAndConditions()
 {
-    var check=document.getElementById("tnc").checked;
-    if(check==false)
+    var check = document.getElementById("tnc").checked;
+    if (check == false)
     {
         alert("Please accept terms and conditions");
+        conditionfield=0;
         return false;
-        field52=0;
     }
     else{
-        field52=1;
+        conditionfield=1;
     }
 
 }
 function pageChange5(){
    
-    console.log(field51);
-    if(field51 == 1 && field52 == 1){
+    if (departmentfield == 1 && departmentfield == 1){
         Submit1.onclick = function(){
     Form5.style.left = "-450px";
     Form6.style.left = "40px";
